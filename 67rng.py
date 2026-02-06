f89202698577a234517ea5cdb0a31ef1d29f890d
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-02-06 07:11:22 -0400

    Create 67rng.py (for the python bros)

---
+++ 67rng.py
@@ -0,0 +1,12 @@
+import random
+
+print("The 67 RNG GAme")
+print("a very stupid idea idk")
+rollsDone = 0
+
+while True:
+  burger = random.randint(1, 1000)
+  print(burger)
+  if burger == 67:
+    print("YOU WON! YOU GET 67")
+    break
