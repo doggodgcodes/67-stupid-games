8cae96c2d757ce26bda0653d7c9a53f1ff5fb691
thedgdog <220253916+doggodgcodes@users.noreply.github.com>
2026-02-06 07:13:16 -0400

    python version

---
+++ 67rng but also 6767rng.py
@@ -0,0 +1,15 @@
+import random
+
+print("The 6767 RNG GAme")
+print("a very stupid idea idk")
+rollsDone = 0
+
+while True:
+  burger = random.randint(1, 10000)
+  print(burger)
+  if burger == 67:
+    print("YOU WON! YOU GET 67")
+    break
+  if burger == 6767:
+    print("YOU WON! YOU GET 6767")
+    break
